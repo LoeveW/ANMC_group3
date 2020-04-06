5ee31756cf7d9377f768278ba75ac00ac981fe8a
Vaurien96 <hugo.laurent96@gmail.com>
2020-04-06 15:12:11 +0200

    playable synth (use space bar)

--- WebInterface/Main.py
+++ WebInterface/Main.py
@@ -1,7 +1,12 @@
 from flask import Flask, escape, request, render_template
 from werkzeug.datastructures import ImmutableMultiDict
+from scipy.io.wavfile import write, read
 
-import sys
+import sys, os
+
+
+from pydub import AudioSegment
+
 
 sys.path.append('..')
 sys.path.append('../RenderMan/Builds/LinuxMakefile/build')
@@ -32,28 +37,35 @@ model.load_state_dict(torch.load("../autoencoder/weights"))
 app = Flask(__name__)
 PORT = 8080
 
+
 # Handler = http.server.SimpleHTTPRequestHandler
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == "POST":
+	if request.method == "POST":
 
-    	# Get the sliders values as integers
-        latent = request.form.to_dict(flat=False).keys()[0]
-        latent = latent.replace('[','').replace(']','').replace('"','')
-        latent = [int(s) for s in latent.split(',')]
-        
-        # Model forward pass with the values as latent space
-        latent = torch.from_numpy(np.array(latent)).float()
-        params = model.decoder(latent).detach()
-        params = np.array(params.detach(), dtype=float)
-        params = fc.syx_to_rm(params)
-        audio = rp.render_patch(params, engine, override=True)
-        print(len(audio))
+		# Get the sliders values as integers
+		latent = request.form.to_dict(flat=False).keys()[0]
+		latent = latent.replace('[','').replace(']','').replace('"','')
+		latent = [float(s) for s in latent.split(',')]
+		
+		# Model forward pass with the values as latent space
+		latent = torch.from_numpy(np.array(latent)).float()
+		params = model.decoder(latent).detach()
+		params = np.array(params.detach(), dtype=float)
+		params = fc.syx_to_rm(params)
+		audio = np.array(rp.render_patch(params, engine, override=True), dtype='float32')
 
-    return render_template("index.html")
+		fname = "static/audio/audio"
+		write(fname+".wav",44100,audio)
 
+		wav = AudioSegment.from_wav(fname+".wav")
+		wav.export(fname+".mp3", format="mp3")
+
+		app.audio_count += 1
+
+	return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080, host='0.0.0.0')
+	app.run(debug=True, port=8080, host='0.0.0.0')
--- WebInterface/static/script.js
+++ WebInterface/static/script.js
@@ -3,9 +3,7 @@ function do_ajax(sliders_values) {
 
     req.open('POST', '/', true);
     req.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
-    console.log(sliders_values)
     var arrString = JSON.stringify(sliders_values);
-    console.log(arrString)
     req.send(arrString);
 
 }
@@ -21,15 +19,16 @@ for (let i = 0; i < n_sliders; i++) {
     sliders[i] = document.createElement('input');
     sliders[i].setAttribute("id", "slider"+i);
     sliders[i].setAttribute("type", 'range');
-    sliders[i].setAttribute("min", 1);
-    sliders[i].setAttribute("max", 100);
-    sliders[i].setAttribute("value", 50);
+    sliders[i].setAttribute("min", -5);
+    sliders[i].setAttribute("max", 5);
+    sliders[i].setAttribute("value", 0);
+    sliders[i].setAttribute("step", 0.05);
     sliders[i].setAttribute("class", "slider");
     slidercontainer.appendChild(sliders[i]);
 
     sliders_values.push(sliders[i].value); // Display the default slider value
 
-    sliders[i].oninput = function () {
+    sliders[i].onmouseup = function () {
         sliders_values[i] = this.value;
         do_ajax(sliders_values);
     };
@@ -37,3 +36,15 @@ for (let i = 0; i < n_sliders; i++) {
 
 }
 do_ajax(sliders_values);
+
+var audioplayer = document.getElementById('audioplayer');
+var audio_count = 0; // serves as cache busting (force browser to reload changing audio file)
+
+document.addEventListener('keypress', play);
+function play(e) {
+  if(e.keyCode == 32){ // check space bar pressed
+        audioplayer.src = "static/audio/audio.mp3?cb="+audio_count;
+        audioplayer.play();
+        audio_count++;
+    }
+}
